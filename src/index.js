bc97aec53e59fe4d9adbb6ed8efbf7fa950b51cc
Maksym Pshenychnyi <maksim_pshenichniy@mail.ru>
2022-08-17 22:58:21 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { PersistGate } from 'redux-persist/es/integration/react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
@@ -10,9 +10,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-            <HashRouter>
+            <BrowserRouter basename="/goit-react-hw-08-phonebook/">
                 <App />
-            </HashRouter>
+            </BrowserRouter>
         </PersistGate>
     </Provider>
 );
